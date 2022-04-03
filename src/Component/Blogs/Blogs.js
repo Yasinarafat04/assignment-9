58a9e1e68db3b68aa4b4518100f40f810d5b6ba2
yasinarafat04 <arafattt05@gmail.com>
2022-04-03 23:35:26 +0530

    allcommit

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -4,6 +4,11 @@ function Blogs() {
     return (
         <div className='mt-5 container'>
             Blog Page
+            <p>1.React context allows us to pass down and use (consume) data in whatever component we need in our React app without using props.
+
+                In other words, React context allows us to share data (state) across our components more easily.</p>
+
+            <p>2.The core characteristic of a semantic element is that it clearly communicated its meaning to both the developer and the browser. These elements clearly define its content. The semantic tag help the search engines and other user devices to determine the importance and context of web pages </p>
         </div>
     )
 }
